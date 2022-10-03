2dec51653f725e6f4863aa002215794372fa5b24
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-03 11:30:27 -0300

    Criando DaoUser

---
+++ dao/DaoUser.php
@@ -0,0 +1,30 @@
+<?php
+
+use Connection as GlobalConnection;
+
+require_once 'Connection.php';
+
+class DaoUser
+{
+    public function insertUser(Users $user)
+    {
+        $sql = 'insert into usuarios (login, nome, cpf, email, senha, telefone)
+        values (?, ?, ?, ?, ?, ?);';
+
+        $pst = GlobalConnection::getPreparedStatement($sql);
+        $pst->bindValue(1, $user->getLogin());
+        $pst->bindValue(2, $user->getName());
+        $pst->bindValue(3, $user->getCpf());
+        $pst->bindValue(4, $user->getEmail());
+        $pst->bindValue(5, $user->getPassword());
+        $pst->bindValue(6, $user->getTelephone());
+
+        if ($pst->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //terminar o resto 
+}
