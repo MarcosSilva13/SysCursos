0591f826019f82dae9ac6ac63ee5a88e690930c9
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-13 15:04:41 -0300

    Edição do painel e menu lateral

--- painel.php
+++ painel.php
@@ -2,6 +2,33 @@
 include 'verifyLogin.php'; //verifica se o usuario esta logado, para nao entrar direto no painel
 //session_abort();
 ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/menuStyle.css">
+    <title>Painel</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="btn-abrir" onclick="openMenu()">&#9776; Abrir</a>
+        <h2>Bem vindo(a): <?= $_SESSION['usuario'];?></h2>
+    </header>
+    <nav id="menu">
+        <a href="#" onclick="closeMenu()">&times; Fechar</a>
+        <a href="#">Home</a>
+        <a href="#">Meus Cursos</a>
+        <a href="#">Alguma coisa</a>
+        <a href="#">Configuração</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+    <main id="content">
+        <h1>SysCursos</h1>
+        <p>Todo conteúdo do sistema aqui.</p>
+    </main>
 
-<h2>Ola, <?= $_SESSION['usuario'];?></h2>
-<h2><a href="logout.php">Sair</a></h2>
+    <!-- JavaScript -->
+    <script src="JS/controleMenu.js"></script>
+</html>
--- view/formAddUser.php
+++ view/formAddUser.php
@@ -11,39 +11,6 @@ session_start();
     <title>Cadastro - Usuário</title>
 </head>
 <body>
-    <script>
-        function mascara(i,atributo) {
-            var v = i.value;
-            if (isNaN(v[v.length-1])) { // impede entrar outro caractere que não seja número
-                i.value = v.substring(0, v.length-1);
-                return;
-            }
-            if (atributo == "cpf") {
-                i.setAttribute("maxlength", "14");
-                if (v.length == 3 || v.length == 7) i.value += ".";
-                if (v.length == 11) i.value += "-";
-            }
-            /*if (atributo == "tele") {
-                i.setAttribute("maxlength", "14"); 
-                //i.value += "(";
-                if (v.length == 2) i.value += ")";
-                if (v.length == 9) i.value += "-";
-            }*/
-        }
-
-        /*function mascaraTel(i) {
-            var v = i.value;
-            if (isNaN(v[v.length-1])) {
-                i.value = v.substring(0, v.length-1);
-                return;
-            }
-            i.setAttribute("maxlength", "14");
-            v[0] = "("; 
-            //i.value += "(";
-            if (v.length == 2) i.value += ")";
-            if (v.length == 9) i.value += "-";
-        }*/
-    </script>
     <div class="area-form-user">
         <div class="form-user">
             <h1>Cadastro</h1>
@@ -109,5 +76,8 @@ session_start();
     <footer>
         <p>Desenvolvido por Marcos Antônio &copy; 2022</p>
     </footer>
+    
+    <!-- JavaScript -->
+    <script src="../JS/mascaraCampos.js"></script>
 </body>
 </html>
