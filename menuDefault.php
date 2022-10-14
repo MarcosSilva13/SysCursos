5c94318a8e1ab51bab78d943b99a38338c4efe07
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-14 11:27:02 -0300

    menu padrao

---
+++ menuDefault.php
@@ -0,0 +1,14 @@
+<header>
+        <a href="#" class="btn-abrir" onclick="openMenu()">&#9776; Abrir</a>
+        <!--<h2>Bem vindo(a): <?= $_SESSION['usuario'];?></h2>-->
+</header>
+    <nav id="menu">
+        <a href="#" onclick="closeMenu()">&times; Fechar</a>
+        <a href="coursesDefault.php"> <i class="fas fa-home"></i>Home</a>
+        <a id="abc" href="coursesAdmin.php"> <i class="fas fa-book"></i>Meus Cursos</a>
+        <a href="#">Alguma coisa</a>
+        <a href="#"> <i class="fa fa-gear"></i>Configuração</a>
+        <a href="logout.php"> <i class="fas fa-power-off"></i>Logout</a>
+    </nav>
+    
+    <!-- fas fa-sign-out-alt -->
