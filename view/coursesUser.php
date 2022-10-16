02ab9838f408392237cd6f5041f30515dca34573
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-16 14:52:33 -0300

    Criando coursesUser

---
+++ view/coursesUser.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="../CSS/coursesDefaultStyle.css">
+    <link rel="stylesheet" href="../CSS/menuStyle.css">
+    <title>Meus Cursos</title>
+</head>
+<body>
+    <?php include 'menuDefault.php'; $id_user = $_SESSION['id_user'];?>
+        <main id="content">
+        <table id="tab">
+                <tr>
+                    <th>Curso</th>
+                    <th>Valor</th>
+                    <th>Duração</th>
+                    <th>Descricao</th>
+                    <th>Ações</th>
+                </tr>
+
+                <?php 
+                    require_once '../dao/DaoSale.php';
+                    $dao = new DaoSale();
+
+                    $list = $dao->listCoursesUser($id_user);
+
+                    foreach ($list as $values) {
+                        echo '<tr>';
+                        echo '<td>' . $values['nome_curso'] . '</td>';
+                        echo '<td>' . $values['valor_curso'] . '</td>';
+                        echo '<td>' . $values['duracao_curso'] . 'H</td>';
+                        echo '<td>' . $values['descricao_curso'] . '</td>';
+                        /*echo '<td>
+                        <form action="formBuyCourse.php" method="POST">
+                        <input type="hidden" name="id_course" id="id_course" value="' . $values['id_curso'] . '"/>
+                        <input type="hidden" name="course_name" id="course_name" value="' . $values['nome_curso'] . '"/>
+                        <input type="hidden" name="course_price" id="price" value="' . $values['valor_curso'] . '"/>
+                        <input type="hidden" name="course_duration" id="course_duration" value="' . $values['duracao_curso'] . '"/>
+                        <input type="submit" id="buy" value="Comprar"/>
+                        </form></td>';*/
+                        echo '</tr>';
+                    }
+                ?>
+            </table>
+        </main>
+
+
+    <script src="../JS/controleMenu.js"></script>
+</body>
+</html>
