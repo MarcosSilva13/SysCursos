e326cda208d5a1cf761916ec6bcc87a16e5582fd
MarcosSilva13 <marcosslp45@gmail.com>
2022-10-19 21:25:02 -0300

    Ajuste no formato da data

--- confirmSale.php
+++ confirmSale.php
@@ -8,14 +8,15 @@ $dao = new DaoSale();
 //valores vindo de formBuyCourse.php
 $id_user = filter_input(INPUT_POST, 'id_user');
 $id_course = filter_input(INPUT_POST, 'id_course');
-$date = date('Y-m-d', time());
+//$date = date('Y-m-d');
+$date = new DateTime('now', new DateTimeZone('America/Sao_Paulo'));
 $payment = filter_input(INPUT_POST, 'payment');
 $final_price = filter_input(INPUT_POST, 'course_price');
 
-echo "$id_user<br>$id_course<br>$date<br>$payment<br>$final_price<br>";
+//echo "$id_user<br>$id_course<br>$date<br>$payment<br>$final_price<br>";
 
 if ($id_user && $id_course && $date && $payment && $final_price) {
-    $obj = new Sales(null, $id_user, $id_course, $date, $payment, $final_price);
+    $obj = new Sales(null, $id_user, $id_course, $date->format('Y-m-d'), $payment, $final_price);
 
     if ($dao->insertSale($obj)) {
         $_SESSION['confirm-sale'] = true; // sessão para notificação de compra
